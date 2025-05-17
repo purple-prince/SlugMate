641553000bb1ebb0e44d68917a3b65bc1ba206f2
purple-prince <himynameischuk44@gmail.com>
2025-05-16 21:08:35 -0700

    Initial Commit

---
+++ SlugMate/SlugMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlugMateApp.swift
+//  SlugMate
+//
+//  Created by Charlie Reeder on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SlugMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlugMateTests/SlugMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlugMateTests.swift
+//  SlugMateTests
+//
+//  Created by Charlie Reeder on 5/16/25.
+//
+
+import Testing
+@testable import SlugMate
+
+struct SlugMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
